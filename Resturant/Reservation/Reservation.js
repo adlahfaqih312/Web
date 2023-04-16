de8f795fbc9213f8f3780f38607c288bd748e372
adlahfaqih312 <108047865+adlahfaqih312@users.noreply.github.com>
2023-04-16 17:53:33 +0300

    Add files via upload

---
+++ Resturant/Reservation/Reservation.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const phone = document.getElementById('phone').value.trim();
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+  const guests = document.getElementById('guests').value;
+  const reservation = { name, email, phone, date, time, guests };
+  console.log(reservation);
+});
